1b2beb3f9b1802af45341e0943faaf9c84d0d055
Rizz <rissfdh@gmail.com>
2025-01-08 08:42:10 +0700

    modifikasi tampilan ui

--- app/src/main/java/com/example/cowdigi/MainActivity.kt
+++ app/src/main/java/com/example/cowdigi/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        enableEdgeToEdge()
 
         // Tombol untuk membuka dialog fragment
         binding.openDialogButton.setOnClickListener {
